444a009d4fde60f50fed3c0aa4356ac988f6b990
Michael Almeida <michaelcardoso97@gmail.com>
2023-05-10 11:56:13 -0300

    CRUD

--- src/app.js
+++ src/app.js
@@ -17,10 +17,33 @@ app.get("/livros", (req, res) => {
   res.status(200).json(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+  let index = buscaLivro(req.params.id);
+  res.status(200).json(livros[index]);
+});
+
 app.post("/livros", (req, res) => {
   livros.push(req.body);
   res.status(201).send("Livro cadastrado com sucesso");
 });
 
+app.put("/livros/:id", (req, res) => {
+  let index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.json(livros);
+});
+
+//função para localizar o livro pelo id
+function buscaLivro(id) {
+  return livros.findIndex((livro) => livro.id == id);
+}
+
 //exportando para que outros arquivos possam utilizar
 export default app;
+
+app.delete("/livros/:id", (req, res) => {
+  let { id } = req.params;
+  let index = buscaLivro(id);
+  livros.splice(index, 1);
+  res.send(`Livro ${id} excluído com sucesso!`);
+});
