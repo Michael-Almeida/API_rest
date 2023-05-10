73a84edee0f9e261ea3f608134ceecf62d7f8ce0
Michael Almeida <michaelcardoso97@gmail.com>
2023-05-10 10:26:41 -0300

    refatorando

--- server.js
+++ server.js
@@ -1,23 +1,9 @@
-//Criando servidor local com módulo nativo do node
-const http = require("http");
+import app from "./src/app.js";
+
 //definir uma porta para escutar requisição
-const porta = 3000;
-
-//Rotas
-const rotas = {
-  "/": "Curso de node",
-  "/livros": "Entrei na página de livros",
-  "/autores": "Listagem de autores",
-  "/sobre": "Infos sobre o projeto",
-};
-
-//Criar o servidor e o que irá escutar
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/plain" });
-  res.end(rotas[req.url]);
-});
+const porta = process.env.PORT || 3000; //Porta do ambiente de produção ou 3000
 
 //fazer o link entre servidor e a porta
-server.listen(porta, () => {
+app.listen(porta, () => {
   console.log(`Servidor escutando em http://localhost:${porta}`);
 });
