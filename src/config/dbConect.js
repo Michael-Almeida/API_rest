da6282f1ac13b38015ab03667b68bf1a97ad2fd7
Michael Almeida <michaelcardoso97@gmail.com>
2023-05-11 10:04:20 -0300

    Ajuste de conexão do banco

--- src/config/dbConect.js
+++ src/config/dbConect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose" 
 
-mongoose.connect("mongodb+srv://michael:123@alura.xja8o3s.mongodb.net/"); //conectando com a string do mongo Atlas
+mongoose.connect("mongodb+srv://michael:123@alura.xja8o3s.mongodb.net/alura-node"); //conectando com a string do mongo Atlas
 
 let db = mongoose.connection;
 
