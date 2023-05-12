0aebc78d6e7e05d3c782876112523c1aa7f9ec12
Michael Almeida <michaelcardoso97@gmail.com>
2023-05-12 12:12:30 -0300

    Refatoração com Try/catch e assync/await

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -3,75 +3,78 @@ import livros from "../models/Livro.js";
 //Métodos http
 
 class LivroController {
-  static listarLivros = (req, res) => {
-    livros
-      .find()
-      .populate("autor")
-      .exec((err, livros) => {
-        res.status(200).json(livros);
+  static listarLivros = async (req, res) => {
+    try {
+      const listaLivros = await livros.find().populate("autor").exec();
+      res.status(200).json(listaLivros);
+    } catch (erro) {
+      res.status(500).json({ message: "Erro interno do servidor" });
+    }
+  };
+
+  static listarLivroPorId = async (req, res) => {
+    try {
+      const id = req.params.id;
+
+      const livroResultadoPorId = await livros
+        .findById(id)
+        .populate("autor", "nome")
+        .exec();
+      res.status(200).send(livroResultadoPorId);
+    } catch (erro) {
+      res
+        .status(400)
+        .send({ message: `${erro.message} - Id do livro não localizado.` });
+    }
+  };
+
+  static cadastrarLivro = async (req, res) => {
+    try {
+      let livro = new livros(req.body);
+
+      const livroSalvo = await livro.save();
+      res.status(201).send(livroSalvo.toJSON());
+    } catch (erro) {
+      res
+        .status(500)
+        .send({ message: `${erro.message} - falha ao cadastrar livro` });
+    }
+  };
+
+  static atualizarLivro = async (req, res) => {
+    try {
+      const id = req.params.id;
+
+      await livros.findByIdAndUpdate(id, {
+        $set: req.body,
       });
+      res.status(200).send({ message: "Livro Cadastrado com sucesso" });
+    } catch (erro) {
+      res.status(500).send({ message: erro.message });
+    }
   };
 
-  static listarLivroPorId = (req, res) => {
-    const id = req.params.id;
+  static excluirLivro = async (req, res) => {
+    try {
+      const id = req.params.id;
 
-    livros
-      .findById(id)
-      .populate("autor", "nome")
-      .exec((err, livros) => {
-        if (err) {
-          res
-            .status(400)
-            .send({ message: `${err.message} - Id do livro não localizado.` });
-        } else {
-          res.status(200).send(livros);
-        }
-      });
+      await livros.findByIdAndDelete(id);
+
+      res.status(200).send({ message: "Livro removido com sucesso!" });
+    } catch (erro) {
+      res.status(500).send({ message: erro.message });
+    }
   };
 
-  static cadastrarLivro = (req, res) => {
-    let livro = new livros(req.body);
-    livro.save((err) => {
-      if (err) {
-        res
-          .status(500)
-          .send({ message: `${err.message} - falha ao cadastrar livro` });
-      } else {
-        res.status(201).send(livro.toJSON());
-      }
-    });
-  };
+  static listarLivroPorEditora = async (req, res) => {
+    try {
+      const editora = req.query.editora;
 
-  static atualizarLivro = (req, res) => {
-    const id = req.params.id;
-
-    livros.findByIdAndUpdate(id, { $set: req.body }, (err) => {
-      if (!err) {
-        res.status(200).send({ message: "livro atualizado com sucesso!" });
-      } else {
-        res.status(500).send({ message: err.message });
-      }
-    });
-  };
-
-  static excluirLivro = (req, res) => {
-    const id = req.params.id;
-
-    livros.findByIdAndDelete(id, (err) => {
-      if (!err) {
-        res.status(200).send({ message: "Livro removido com sucesso!" });
-      } else {
-        res.status(500).send({ message: err.message });
-      }
-    });
-  };
-
-  static listarLivroPorEditora = (req, res) => {
-    const editora = req.query.editora;
-
-    livros.find({ 'editora': editora }, {}, (err, livros) => {
-      res.status(200).send(livros);
-    });
+      const livroResultado = await livros.find({ editora: editora });
+      res.status(200).send(livroResultado);
+    } catch (error) {
+      res.status(500).send({ message: "Erro interno no servidor" });
+    }
   };
 }
 
