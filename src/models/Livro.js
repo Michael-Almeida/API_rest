9ca85e34e67529b6b4cac47f3e31864b41cebd2d
Michael Almeida <michaelcardoso97@gmail.com>
2023-05-12 15:58:36 -0300

    Tratando os erros de forma mais amigável

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -4,14 +4,14 @@ import mongoose from "mongoose";
 
 const livroSchema = new mongoose.Schema({
   id: { type: String },
-  titulo: { type: String, required: true },
+  titulo: { type: String, required: [true, "o título do livro é obrigatório"] },
   autor: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "autores",
-    required: true,
+    required: [true, "O(a) autor(a) é obrigatório"]
   },
-  editora: { type: String, required: true },
-  numeroPaginas: { type: Number, required: true },
+  editora: { type: String, required: [true, "O(a) editor(a) é obrigatório"] },
+  numeroPaginas: { type: Number, required: [true, "o número de páginas é obrigatório"] },
 });
 
 const livros = mongoose.model("livros", livroSchema);
